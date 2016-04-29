cacf39b56440f39542c158a773f9b287776edcc8
tomasgordian <tomasgordian@gmail.com>
2016-04-29 15:10:23 +0200

    employee class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -2,4 +2,23 @@
  * Created by Tomas on 29.04.2016.
  */
 public class Employee {
+
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }

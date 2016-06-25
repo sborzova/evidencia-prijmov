04680b67585ff0d7a1c2ac2e0266257f7c43a360
tomasgordian <tomasgordian@gmail.com>
2016-06-26 01:58:02 +0200

    listallinvoices

--- src/main/java/backend/InvoiceManager.java
+++ src/main/java/backend/InvoiceManager.java
@@ -3,6 +3,7 @@ package backend;
 import FileProcessing.CreateXMLImpl;
 import FileProcessing.ToPDFImpl;
 import org.apache.fop.apps.FOPException;
+import org.exist.xmldb.EXistResource;
 import org.xmldb.api.base.*;
 import org.xmldb.api.modules.XMLResource;
 import org.xmldb.api.modules.XPathQueryService;
@@ -10,7 +11,9 @@ import org.xmldb.api.modules.XPathQueryService;
 import javax.xml.transform.TransformerException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,53 +40,53 @@ public class InvoiceManager {
 
         new ToPDFImpl().convertToPDF(file);
     }
-/*
+
+
     public List<Invoice> listAllInvoices() throws XMLDBException {
         List<Invoice> invoices = new ArrayList<>();
 
         XPathQueryService xpqs = (XPathQueryService)collection.getService("XPathQueryService", "1.0");
         xpqs.setProperty("indent", "yes");
 
-
-        File directory = new File("\\");
+        File directory = new File(".\\invoices");
 
         for(File file : directory.listFiles()) {
-            ResourceSet result = xpqs.query("/" + file.getName() + "/book/child::node()/text()");
-
-
-
-
             Invoice invoice = new Invoice();
-            invoice.setId();
-            invoice.setEmployeeID();
+
+            ResourceSet result;
+            result = xpqs.query("/" + file.getName() + "/book/iid/text()");
+            ResourceIterator i = result.getIterator();
+            Resource res = null;
+            res = i.nextResource();
+            invoice.setId(Long.parseLong(res.getContent().toString()));
+
+            invoices.add(invoice);
         }
 
         return invoices;
     }
-*/
+
     public Invoice getInvoice(Long id) {
         Invoice invoice = new Invoice();
-
-
-
 
         return invoice;
     }
 
     public void generateDocBook(Invoice invoice) throws XMLDBException {
 
-        XMLResource res = null;
-        res = (XMLResource)collection.createResource(null, "XMLResource");
-
-
         Employee employee = new EmployeeManager(collection).getEmployee(invoice.getEmployeeID());
 
         File f = new CreateXMLImpl().createXML(employee,invoice.getFrom(),invoice.getTo(),
                 new RevenueManager(collection).listRevenuesByDate(employee,invoice.getFrom(),invoice.getTo()));
-        File newFile = new File("\\invoices\\ahoj.dbk");
+        File newFile = new File(".\\invoices\\aho.dbk");
         f.renameTo(newFile);
 
-        res.setContent(newFile);
-        collection.storeResource(res);
+        File directory = new File(".\\invoices");
+
+        Integer size = directory.listFiles().length;
+        size++;
+        Long sizeLong = Long.parseLong(size.toString());
+
+        invoice.setId(sizeLong);
     }
 }

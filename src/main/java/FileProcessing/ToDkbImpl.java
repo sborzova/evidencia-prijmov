bdb124d2458a00d3daba0f559ef184db77ce804a
Martina Cvincekova <martina.cvincekova@gmail.com>
2016-06-25 04:15:23 +0200

    FileProcessing New

--- src/main/java/FileProcessing/ToDkbImpl.java
+++ src/main/java/FileProcessing/ToDkbImpl.java
@@ -1,9 +1,27 @@
 package FileProcessing;
 
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+
 /**
  * Created by Anonym on 24. 6. 2016.
  */
 public class ToDkbImpl {
+    public static void main(String[] args)
+            throws TransformerConfigurationException, TransformerException {
 
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer xsltProc = tf.newTransformer(
+                new StreamSource(new File("C:\\Users\\Anonym\\Documents\\GitHub\\EvidenciaPrijmov\\evidencia-prijmov\\src\\main\\resources\\xmlTemplate.xsl")));
+
+        xsltProc.transform(
+                new StreamSource(new File("C:\\Users\\Anonym\\Documents\\GitHub\\EvidenciaPrijmov\\evidencia-prijmov\\src\\main\\resources\\file.xml")),
+                new StreamResult(new File("C:\\Users\\Anonym\\Documents\\GitHub\\EvidenciaPrijmov\\evidencia-prijmov\\src\\main\\resources\\Invoice.xml")));
+    }
 
 }

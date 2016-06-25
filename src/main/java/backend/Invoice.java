0bef9f027941f8fe34174f832cf6bc1c6e5b225c
marek.scholtz <433335@mail.muni.cz>
2016-06-25 22:21:42 +0200

    hippotomatus13

---
+++ src/main/java/backend/Invoice.java
@@ -0,0 +1,57 @@
+package backend;
+
+import java.time.LocalDate;
+
+/**
+ * @author Marek Scholtz
+ * @version 2016.06.25
+ */
+public class Invoice {
+
+    private Long id;
+    private Long employeeID;
+    private LocalDate from;
+    private LocalDate to;
+
+    public Invoice(Long id, Long employeeID, LocalDate from, LocalDate to) {
+        this.id = id;
+        this.employeeID = employeeID;
+        this.from = from;
+        this.to = to;
+    }
+
+    public Invoice() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(Long employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public LocalDate getFrom() {
+        return from;
+    }
+
+    public void setFrom(LocalDate from) {
+        this.from = from;
+    }
+
+    public LocalDate getTo() {
+        return to;
+    }
+
+    public void setTo(LocalDate to) {
+        this.to = to;
+    }
+}
---
+++ src/main/java/backend/InvoiceManager.java
@@ -0,0 +1,30 @@
+package backend;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Marek Scholtz
+ * @version 2016.06.25
+ */
+public class InvoiceManager {
+
+
+    public void exportToPDF(Invoice invoice) {
+
+
+    }
+    
+    public List<Invoice> listAllInvoices() {
+        List<Invoice> invoices = new ArrayList<>();
+        
+        return invoices;
+    }
+
+    public Invoice getInvoice(Long id) {
+        Invoice invoice = new Invoice();
+
+        return invoice;
+    }
+    
+}

7da87d2e9db42d2c299aef9f580e8d2b3951834f
Martina Cvincekova <martina.cvincekova@gmail.com>
2016-06-25 19:01:22 +0200

    create dbk

--- src/main/java/FileProcessing/ToDkb.java
+++ src/main/java/FileProcessing/ToDkb.java
@@ -1,7 +1,12 @@
 package FileProcessing;
 
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+
 /**
  * Created by Anonym on 24. 6. 2016.
  */
-public class ToDkb {
+public interface ToDkb {
+
+    public void toDbk() throws TransformerException;
 }
--- src/main/java/FileProcessing/ToDkbImpl.java
+++ src/main/java/FileProcessing/ToDkbImpl.java
@@ -1,6 +1,7 @@
 package FileProcessing;
 
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
@@ -10,17 +11,17 @@ import java.io.File;
 /**
  * Created by Anonym on 24. 6. 2016.
  */
-public class ToDkbImpl {
-    public static void main(String[] args)
-            throws TransformerException {
 
+public class ToDkbImpl implements ToDkb{
+
+    @Override
+    public void toDbk() throws TransformerException {
         TransformerFactory tf = TransformerFactory.newInstance();
         Transformer xsltProc = tf.newTransformer(
-                new StreamSource(new File("C:\\Users\\Anonym\\Documents\\GitHub\\EvidenciaPrijmov\\evidencia-prijmov\\src\\main\\resources\\xmlTemplate.xsl")));
+                new StreamSource(new File(".\\src\\main\\resources\\xmlTemplate.xsl")));
 
         xsltProc.transform(
-                new StreamSource(new File("C:\\Users\\Anonym\\Documents\\GitHub\\EvidenciaPrijmov\\evidencia-prijmov\\src\\main\\resources\\file.xml")),
-                new StreamResult(new File("C:\\Users\\Anonym\\Documents\\GitHub\\EvidenciaPrijmov\\evidencia-prijmov\\src\\main\\resources\\Invoice.xml")));
+                new StreamSource(new File(".\\src\\main\\resources\\revenueXml.xml")),
+                new StreamResult(new File(".\\src\\main\\resources\\Invoice.xml")));
     }
-
 }
